7613c6a400c214ca449b3690cf6b0176fac65168
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-12 08:45:53 +0000

    Add image validation to restaurant image field

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -4,6 +4,26 @@ from .models import Restaurant
 
 class RestaurantSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='created_by.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB.'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px.'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px.'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Restaurant
