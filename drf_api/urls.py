8718d661ee1fa295e22af0099a3fdf7e2ad02d19
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-12 08:33:48 +0000

    Add permissions.py, add is_owner to profiles

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include ('restaurants.urls'))
 ]
