32d5b4f85967c0fe296375c0bb504302173e8522
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-13 15:02:29 +0000

    Refactor like views to work with generics

--- likes/views.py
+++ likes/views.py
@@ -1,60 +1,21 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from .models import Like
 from .serializers import LikeSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class LikeList(APIView):
+class LikeList(generics.ListCreateAPIView):
     serializer_class = LikeSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
+    queryset = Like.objects.all()
 
-    def get(self, request):
-        likes = Like.objects.all()
-        serializer = LikeSerializer(
-            likes, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = LikeSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(created_by=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
 
 
-class LikeDetail(APIView):
+class LikeDetail(generics.RetrieveDestroyAPIView):
     serializer_class = LikeSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            like = Like.objects.get(pk=pk)
-            self.check_object_permissions(self.request, like)
-            return like
-        except Like.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        like = self.get_object(pk)
-        serializer = LikeSerializer(
-            like, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def delete(self, request, pk):
-        like = self.get_object(pk)
-        like.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Like.objects.all()
