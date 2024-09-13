dc016280327f1154b398e8b450476b98c49a2bcd
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-13 08:57:01 +0000

    Add image validation to DishSerializers

--- dishes/serializers.py
+++ dishes/serializers.py
@@ -4,6 +4,21 @@ from .models import Dish
 
 class DishSerializer(serializers.ModelSerializer):
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB.'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px.'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px.'
+            )
+        return value
+
     class Meta:
         model = Dish
         fields = '__all__'
