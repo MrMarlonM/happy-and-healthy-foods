42f72054803594799a99124b02a3ae97d2037499
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-15 07:56:26 +0000

    Add custom update method to profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
+from restaurants.models import Restaurant
 
 
 class ProfileList(generics.ListAPIView):
@@ -13,3 +15,23 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        profile = self.get_object()
+        restaurant_id = request.data.get('restaurant_id')
+
+        try:
+            restaurant = Restaurant.objects.get(id=restaurant_id)
+        except Restaurant.DoesNotExist:
+            return Response(
+                {'error': 'Restaurant does not exist'}, 
+                status=status.HTTP_404_NOT_FOUND
+                )
+        
+        if request.method == 'PUT':
+            profile.favorites.add(restaurant)
+        elif request.method == 'DELETE':
+            profile.favorites.remove(restaurant)
+        
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data)
