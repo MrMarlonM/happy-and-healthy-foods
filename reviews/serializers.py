82189a749a3cd2b9693136650f57908a76643994
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-17 09:55:45 +0000

    Change datetime fields, add rootroute

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Review
 
@@ -5,11 +6,19 @@ from .models import Review
 class ReviewSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source='created_by.username')
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.created_by
     
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
     class Meta:
         model = Review
         fields = '__all__'
