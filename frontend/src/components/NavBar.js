269ddc32cb06bd177872319c9ab409463e78c18a
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-18 08:02:05 +0000

    Add custom style to App.js and NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
     return (
-        <Navbar expand="md">
+        <Navbar className={styles.NavBar} expand="md" fixed='top'>
             <Container>
-                <Navbar.Brand>Happy and Healthy Food's</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
@@ -13,11 +13,8 @@ const NavBar = () => {
                         <Nav.Link>Sign In</Nav.Link>
                         <Nav.Link>Sign Up</Nav.Link>
                     </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
                 </Navbar.Collapse>
+                <Navbar.Brand>Happy and Healthy Food's</Navbar.Brand>
             </Container>
         </Navbar>
     )
