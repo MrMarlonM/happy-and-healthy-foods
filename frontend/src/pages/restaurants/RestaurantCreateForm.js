d3bdd95410e731f6bdb2a7f572541e468a838b87
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-20 08:55:13 +0000

    Add functionality to create new restaurant

--- frontend/src/pages/restaurants/RestaurantCreateForm.js
+++ frontend/src/pages/restaurants/RestaurantCreateForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../../api/axiosDefaults';
 
 const RestaurantCreateForm = () => {
     const [errors, setErrors] = useState({});
@@ -10,9 +12,12 @@ const RestaurantCreateForm = () => {
         country: "",
         image: "",
         short_description: "",
-        cuisine_type: "",
+        cuisine_type: "italian",
     });
     const { name, city, country, image, short_description, cuisine_type } = restaurantData;
+
+    const imageInput = useRef(null);
+    const history = useHistory();
 
     const handleChange = (event) => {
         setRestaurantData({
@@ -31,8 +36,30 @@ const RestaurantCreateForm = () => {
         };
     };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const formData = new FormData();
+
+        formData.append('name', name)
+        formData.append('city', city)
+        formData.append('country', country)
+        formData.append('image', imageInput.current.files[0])
+        formData.append('short_description', short_description)
+        formData.append('cuisine_type', cuisine_type)
+
+        try {
+            const {data} = await axiosReq.post('/restaurants/', formData);
+            history.push(`/restaurants/${data.id}`)
+        } catch(err) {
+            console.log(err);
+            if (err.response?.status !== 401){
+                setErrors(err.response?.data);
+            }
+        }
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group>
                 <Form.Label>Name</Form.Label>
                 <Form.Control 
@@ -68,6 +95,7 @@ const RestaurantCreateForm = () => {
                 label="Click here to upload an image of the restaurant"
                 accept='image/*'
                 onChange={handleChangeImage}
+                ref={imageInput}
             />
             <Form.Group>
                 <Form.Label>Short description</Form.Label>
@@ -88,19 +116,19 @@ const RestaurantCreateForm = () => {
                     value={cuisine_type}
                     onChange={handleChange}
                 >
-                    <option>Italian</option>
-                    <option>Indian</option>
-                    <option>German</option>
-                    <option>Japanese</option>
-                    <option>Chinese</option>
-                    <option>Thai</option>
-                    <option>Mexican</option>
-                    <option>Greek</option>
-                    <option>French</option>
-                    <option>Spanish</option>
-                    <option>Vietnamese</option>
-                    <option>Korean</option>
-                    <option>Other</option>
+                    <option value="italian">Italian</option>
+                    <option value="indian">Indian</option>
+                    <option value="german">German</option>
+                    <option value="japanese">Japanese</option>
+                    <option value="chinese">Chinese</option>
+                    <option value="thai">Thai</option>
+                    <option value="mexican">Mexican</option>
+                    <option value="greek">Greek</option>
+                    <option value="french">French</option>
+                    <option value="spanish">Spanish</option>
+                    <option value="vietnamese">Vietnamese</option>
+                    <option value="korean">Korean</option>
+                    <option value="other">Other</option>
                 </Form.Control>
             </Form.Group>
             <Button variant="primary" type="submit">
