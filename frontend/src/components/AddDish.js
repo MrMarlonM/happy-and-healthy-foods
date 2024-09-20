4657e078c99d6ac5817140e0d4aa0a5843ec4a51
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-20 09:35:53 +0000

    Add form for adding dish

---
+++ frontend/src/components/AddDish.js
@@ -0,0 +1,123 @@
+import React, { useRef, useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../api/axiosDefaults';
+
+const AddDish = () => {
+    const [errors, setErrors] = useState({});
+
+    const [dishData, setDishData] = useState({
+        name: "",
+        short_description: "",
+        price: 0,
+        image: "",
+        dietary_preference: "vegetarian",
+    });
+    const { name, short_description, price, image, dietary_preference } = dishData;
+
+    const imageInput = useRef(null);
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setDishData({
+            ...dishData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleChangeImage = (event) => {
+        if (event.target.files.length) {
+            URL.revokeObjectURL(image);
+            setDishData({
+                ...dishData,
+                image: URL.createObjectURL(event.target.files[0])
+            });
+        };
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const formDataDish = new FormData();
+
+        formDataDish.append('name', name)
+        formDataDish.append('short_description', short_description)
+        formDataDish.append('price', price)
+        formDataDish.append('image', imageInput.current.files[0])
+        formDataDish.append('dietary_preference', dietary_preference)
+
+        try {
+            await axiosReq.post('/dishes/', formDataDish);
+        } catch(err) {
+            console.log(err);
+            if (err.response?.status !== 401){
+                setErrors(err.response?.data);
+            }
+        }
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group>
+                <Form.Label>Name</Form.Label>
+                <Form.Control 
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={handleChange} 
+                    placeholder="Name of the dish"
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Short description</Form.Label>
+                <Form.Control 
+                    as="textarea"
+                    rows={3}
+                    name="short_description"
+                    value={short_description}
+                    onChange={handleChange}
+                    placeholder="Add a short description here"
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Price</Form.Label>
+                <Form.Control
+                    type="number"
+                    name="price"
+                    value={price}
+                    onChange={handleChange}
+                    placeholder="€"
+                />
+            </Form.Group>
+            <Form.File
+                id="image-upload-dish"
+                label="Click here to upload an image of the restaurant"
+                accept='image/*'
+                onChange={handleChangeImage}
+                ref={imageInput}
+            />
+            <Form.Group>
+                <Form.Label>dietary Preference</Form.Label>
+                <Form.Control
+                    as="select"
+                    name="dietary_preference"
+                    value={dietary_preference}
+                    onChange={handleChange}
+                >
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="vegan">Vegan</option>
+                    <option value="pescatarian">Pescatarian</option>
+                    <option value="gluten-free">Gluten-free</option>
+                    <option value="halal">Halal</option>
+                    <option value="kosher">Kosher</option>
+                    <option value="other">other</option>
+                </Form.Control>
+            </Form.Group>
+            <AddDish/>
+            <Button variant="primary" type="submit">
+                Add Dish
+            </Button>
+        </Form>
+    )
+}
+
+export default AddDish
--- frontend/src/pages/restaurants/RestaurantCreateForm.js
+++ frontend/src/pages/restaurants/RestaurantCreateForm.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { axiosReq } from '../../api/axiosDefaults';
+import AddDish from '../../components/AddDish';
 
 const RestaurantCreateForm = () => {
     const [errors, setErrors] = useState({});
@@ -48,93 +49,102 @@ const RestaurantCreateForm = () => {
         formData.append('cuisine_type', cuisine_type)
 
         try {
-            const {data} = await axiosReq.post('/restaurants/', formData);
+            const { data } = await axiosReq.post('/restaurants/', formData);
             history.push(`/restaurants/${data.id}`)
-        } catch(err) {
+        } catch (err) {
             console.log(err);
-            if (err.response?.status !== 401){
+            if (err.response?.status !== 401) {
                 setErrors(err.response?.data);
             }
         }
     }
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>Name</Form.Label>
-                <Form.Control 
-                    type="text"
-                    name="name"
-                    value={name}
-                    onChange={handleChange} 
-                    placeholder="Name of the restaurant"
+        <>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={handleChange}
+                        placeholder="Name of the restaurant"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="city"
+                        value={city}
+                        onChange={handleChange}
+                        placeholder="City of the restaurant"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Country</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="country"
+                        value={country}
+                        onChange={handleChange}
+                        placeholder="Country of the restaurant"
+                    />
+                </Form.Group>
+                <Form.File
+                    id="image-upload"
+                    label="Click here to upload an image of the restaurant"
+                    accept='image/*'
+                    onChange={handleChangeImage}
+                    ref={imageInput}
                 />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>City</Form.Label>
-                <Form.Control 
-                    type="text"
-                    name="city"
-                    value={city}
-                    onChange={handleChange}
-                    placeholder="City of the restaurant"
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Country</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="country"
-                    value={country}
-                    onChange={handleChange}
-                    placeholder="Country of the restaurant"
-                />
-            </Form.Group>
-            <Form.File
-                id="image-upload"
-                label="Click here to upload an image of the restaurant"
-                accept='image/*'
-                onChange={handleChangeImage}
-                ref={imageInput}
-            />
-            <Form.Group>
-                <Form.Label>Short description</Form.Label>
-                <Form.Control 
-                    as="textarea"
-                    rows={3}
-                    name="short_description"
-                    value={short_description}
-                    onChange={handleChange}
-                    placeholder="Add a short description here"
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Cuisine Type</Form.Label>
-                <Form.Control
-                    as="select"
-                    name="cuisine_type"
-                    value={cuisine_type}
-                    onChange={handleChange}
+                <Form.Group>
+                    <Form.Label>Short description</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                        name="short_description"
+                        value={short_description}
+                        onChange={handleChange}
+                        placeholder="Add a short description here"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Cuisine Type</Form.Label>
+                    <Form.Control
+                        as="select"
+                        name="cuisine_type"
+                        value={cuisine_type}
+                        onChange={handleChange}
+                    >
+                        <option value="italian">Italian</option>
+                        <option value="indian">Indian</option>
+                        <option value="german">German</option>
+                        <option value="japanese">Japanese</option>
+                        <option value="chinese">Chinese</option>
+                        <option value="thai">Thai</option>
+                        <option value="mexican">Mexican</option>
+                        <option value="greek">Greek</option>
+                        <option value="french">French</option>
+                        <option value="spanish">Spanish</option>
+                        <option value="vietnamese">Vietnamese</option>
+                        <option value="korean">Korean</option>
+                        <option value="other">Other</option>
+                    </Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add restaurant
+                </Button>
+                <Button
+                    className="mx-2"
+                    variant="primary"
+                    onClick={() => history.goBack()}
                 >
-                    <option value="italian">Italian</option>
-                    <option value="indian">Indian</option>
-                    <option value="german">German</option>
-                    <option value="japanese">Japanese</option>
-                    <option value="chinese">Chinese</option>
-                    <option value="thai">Thai</option>
-                    <option value="mexican">Mexican</option>
-                    <option value="greek">Greek</option>
-                    <option value="french">French</option>
-                    <option value="spanish">Spanish</option>
-                    <option value="vietnamese">Vietnamese</option>
-                    <option value="korean">Korean</option>
-                    <option value="other">Other</option>
-                </Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+                    Cancel
+                </Button>
+            </Form>
+        </>
     )
 }
 
