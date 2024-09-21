0f9902234ed9b02854cc8be3f75266224aee96b1
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-21 08:55:12 +0000

    Add logic to present reviews and dishes on RestaurantPage

--- frontend/src/pages/restaurants/RestaurantPage.js
+++ frontend/src/pages/restaurants/RestaurantPage.js
@@ -9,16 +9,26 @@ import AddReview from '../../components/AddReview';
 
 const RestaurantPage = () => {
   const { id } = useParams();
-  const [restaurant, setRestaurant] = useState({ results: [] })
-  const { created_by } = {...restaurant.results[0]};
+  const [restaurant, setRestaurant] = useState({ results: [] });
+  const [dishes, setDishes] = useState({ results: [] });
+  const [reviews, setReviews] = useState({results: []});
+  const { created_by } = { ...restaurant.results[0] };
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: restaurant }] = await Promise.all([
-          axiosReq.get(`/restaurants/${id}`)
-        ]);
+        const [
+          { data: restaurant },
+          { data: dishes },
+          { data: reviews },
+        ] = await Promise.all([
+            axiosReq.get(`/restaurants/${id}`),
+            axiosReq.get(`/dishes/?restaurant=${id}`),
+            axiosReq.get(`/reviews/?restaurant=${id}`),
+          ]);
         setRestaurant({ results: [restaurant] });
+        setDishes(dishes);
+        setReviews(reviews);
       } catch (err) {
         console.log(err);
       }
@@ -34,10 +44,20 @@ const RestaurantPage = () => {
       <Restaurant {...restaurant.results[0]} />
       <Row>
         <Col>
-          {is_creator && <AddDish />}
+          {is_creator ? <AddDish /> : "Dishes"}
+          {dishes.results.length ? (
+            dishes.results.map(dish => (
+              <p>{dish.name}</p>
+            ))
+          ) : "No dishes added yet..."}
         </Col>
         <Col>
-          <AddReview/>
+          {currentUser ? <AddReview /> : "Reviews"}
+          {reviews.results.length ? (
+            reviews.results.map(review => (
+              <p>{review.content}</p>
+            ))
+          ) : "No reviews yet, be the first to write one!"}
         </Col>
       </Row>
     </>
