b1c9a03642de3eed4b1e0e6e88d81aad7107aee9
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-22 15:57:59 +0000

    Add ability to search through restaurant lists

--- frontend/src/pages/restaurants/RestaurantList.js
+++ frontend/src/pages/restaurants/RestaurantList.js
@@ -2,27 +2,46 @@ import React, { useEffect, useState } from 'react'
 import { axiosReq } from '../../api/axiosDefaults';
 import Restaurant from './Restaurant';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { Form } from 'react-bootstrap';
 
-const RestaurantList = ({filter=""}) => {
+const RestaurantList = ({ filter = "" }) => {
     const [restaurants, setRestaurants] = useState({ results: [] });
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const fetchRestaurants = async () => {
             try {
-                const { data } = await axiosReq.get(`/restaurants/?${filter}`);
-                setRestaurants(data);
+              const params = new URLSearchParams(); 
+              if (filter) {
+                params.append('created_by', filter); 
+              }
+              if (query) {
+                params.append('search', query);
+              }
+              const { data } = await axiosReq.get(`/restaurants/?${params.toString()}`); 
+              setRestaurants(data);
             } catch (err) {
-                console.log(err)
+              console.log(err);
             }
-        }
-        fetchRestaurants();
-    }, [filter, pathname])
+          };
+          fetchRestaurants();
+    }, [filter, pathname, query])
 
     return (
         <>
+            <Form
+                onSubmit={(event) => event.preventDefault()}
+            >
+                <Form.Control
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                    type="text"
+                    placeholder="search posts"
+                />
+            </Form>
             {restaurants?.results.map(restaurant => (
-                <Restaurant key={restaurant.id} {...restaurant}/>
+                <Restaurant key={restaurant.id} {...restaurant} />
             ))}
         </>
     )
