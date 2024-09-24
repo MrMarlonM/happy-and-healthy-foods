730ed11305012c9382ea88d7883fe8309a80414e
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-24 13:45:47 +0000

    Add modal for delete confirmation on reviews

--- frontend/src/components/reviews/Review.js
+++ frontend/src/components/reviews/Review.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react'
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Modal } from 'react-bootstrap';
 import { axiosRes } from '../../api/axiosDefaults';
 import EditReview from './EditReview';
 
 const Review = (props) => {
     const currentUser = useCurrentUser();
+    const [show, setShow] = useState(false);
 
     const {
         id,
         created_by,
         updated_at,
         content,
-        restaurant,
         setReviews,
     } = props;
     const [showEditForm, setShowEditForm] = useState(false);
 
     const is_creator = currentUser.username === created_by;
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
 
     const handleDelete = async () => {
         try {
@@ -32,32 +35,48 @@ const Review = (props) => {
     }
 
     return (
-        <div>
-            {showEditForm ? (
-                <EditReview
-                    content={content}
-                    setShowEditForm={setShowEditForm}
-                    id={id}
-                    setReviews={setReviews}
-                />
-            ) :
-                <Card className="my-2">
-                    <Card.Body>
-                        <Card.Subtitle className="mb-2 text-muted">
-                            <span className='mx-1'>{created_by}</span>
-                            <span className='mx-1'>{updated_at}</span>
-                        </Card.Subtitle>
-                        <Card.Text>
-                            {content}
-                        </Card.Text>
-                        {is_creator && !showEditForm && <div>
-                            <Button className='mx-1' onClick={() => setShowEditForm(true)}>Edit</Button>
-                            <Button variant='danger' className='mx-1' onClick={handleDelete}>Delete</Button>
-                        </div>}
-                    </Card.Body>
-                </Card>
-            }
-        </div>
+        <>
+            <div>
+                {showEditForm ? (
+                    <EditReview
+                        content={content}
+                        setShowEditForm={setShowEditForm}
+                        id={id}
+                        setReviews={setReviews}
+                    />
+                ) :
+                    <Card className="my-2">
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                <span className='mx-1'>{created_by}</span>
+                                <span className='mx-1'>{updated_at}</span>
+                            </Card.Subtitle>
+                            <Card.Text>
+                                {content}
+                            </Card.Text>
+                            {is_creator && !showEditForm && <div>
+                                <Button className='mx-1' onClick={() => setShowEditForm(true)}>Edit</Button>
+                                <Button variant='danger' className='mx-1' onClick={handleShow}>Delete</Button>
+                            </div>}
+                        </Card.Body>
+                    </Card>
+                }
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to delete your review?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleDelete}>
+                        Confirm
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     )
 }
 
