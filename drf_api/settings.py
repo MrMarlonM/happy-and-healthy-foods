054656fc872eabdb1df79ecf3dd639fbb3675243
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-09-24 16:41:14 +0000

    Update settings.py databaseurl

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -148,7 +148,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL").encode())
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 
